8b30478a15b741163860eb7bda7fddbf65a88ece
hy30n80 <jjun8030@gist.ac.kr>
2023-04-06 13:10:25 +0900

    불필요 파일 삭제

--- test.py
+++
@@ -1,7 +0,0 @@
-#597개 동작시간이 너무 오래걸려서, csv 파일에 예측값 저장
-import csv 
-
-predict = ["23423","dsfsf","egwrgw","erwee"]
-with open("test_list.csv",'w',newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(predict)
